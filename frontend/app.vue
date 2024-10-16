af91b09dbba73bea7c403fe23d1be55b0e0837ce
Chlebik Krisztofer <145597465+kriszsusu@users.noreply.github.com>
2024-10-16 09:41:04 +0200

    Add files via upload

---
+++ frontend/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtPage />
+</template>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  min-height: 100vh;
+}
+</style>
---
+++ frontend/components/index/Hero.vue
@@ -0,0 +1,11 @@
+<template>
+    <section>
+        <h1>Nincs időm elkezdeni rendesen, de első commit</h1>
+    </section>
+</template>
+
+<style scoped>
+section {
+    text-align: center;
+}
+</style>
